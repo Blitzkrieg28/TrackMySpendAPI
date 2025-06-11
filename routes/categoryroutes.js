80af2f7754b1923e60a5c7fcab2dff756152f08b
Tanmay <tanatwork2809@gmail.com>
2025-06-12 04:17:45 +0530

    some more swagger added

--- routes/categoryroutes.js
+++ routes/categoryroutes.js
@@ -14,6 +14,109 @@ global.expenseCategory= "";
 global.incomeCategory= "";
 
 //router.use(tokenVerificationMiddleware);
+/**
+ * @swagger
+ * /viewexpensecategory:
+ *   get:
+ *     summary: View all expenses in a specific category
+ *     description: Returns a list of expenses matching the given category.
+ *     parameters:
+ *       - in: query
+ *         name: category
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Expense category to filter
+ *     responses:
+ *       200:
+ *         description: List of matching expenses
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 categories:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Expense'
+ */
+
+/**
+ * @swagger
+ * /viewincomecategory:
+ *   get:
+ *     summary: View all incomes in a specific category
+ *     description: Returns a list of incomes matching the given category.
+ *     parameters:
+ *       - in: query
+ *         name: category
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Income category to filter
+ *     responses:
+ *       200:
+ *         description: List of matching incomes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 categories:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Income'
+ */
+
+/**
+ * @swagger
+ * /totalexpensecategory:
+ *   get:
+ *     summary: Get total expense for a specific category
+ *     description: Calculates and returns the total amount spent for the specified expense category.
+ *     parameters:
+ *       - in: query
+ *         name: category
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Expense category
+ *     responses:
+ *       200:
+ *         description: Total expense amount for the category
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 totalexpensecategory:
+ *                   type: number
+ */
+
+/**
+ * @swagger
+ * /totalincomecategory:
+ *   get:
+ *     summary: Get total income for a specific category
+ *     description: Calculates and returns the total income for the specified income category.
+ *     parameters:
+ *       - in: query
+ *         name: category
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Income category
+ *     responses:
+ *       200:
+ *         description: Total income amount for the category
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 totalincomecategory:
+ *                   type: number
+ */
 
 router.get("/viewexpensecategory" , async function(req,res){
   
